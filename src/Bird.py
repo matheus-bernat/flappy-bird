f42d60b49e7bbef3578751f24b8ad37367c9cb99
Matheus Bernat <matvi959@student.liu.se>
2020-01-31 22:21:51 +0100

    Initial files

---
+++ src/Bird.py
@@ -0,0 +1,24 @@
+
+GRAVITY = 10
+
+class Bird:
+    def __init__(self, x_pos, y_pos, y_vel, alive, width, height, score):
+        self.bird_width = width
+        self.bird_height = height
+        self.alive = True
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.y_vel = y_vel
+        self.score = score
+
+    def flap(self):
+        self.y_pos -= y_vel
+
+    def fall(self):
+        self.y_pos += y_vel
+
+    def update_score(self):
+        self.score += 1
+
+    def kill_flappy(self):
+        self.alive = False

cea3ad5cf0ff94ad90e4d74db0b079d1af32f7ba
Filip Berglund <filbe882@student.liu.se>
2020-02-01 10:43:52 +0100

    Implemented frame rate cap

--- src/GameLoop.py
+++ src/GameLoop.py
@@ -9,14 +9,15 @@ class GameLoop:
         pygame.init()
         self.window = pygame.display.set_mode((WIDTH, HEIGHT))
         self.game_state = GameState(self.window)
-        clock = pygame.time.Clock()
 
     def update(self):
         self.game_state.update()
 
 def main():
     game = GameLoop()
+    clock = pygame.time.Clock()
     while game.game_state.running:
+        clock.tick(60)
         game.update()
         pygame.display.flip()
 

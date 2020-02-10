9506380c911ffe3d564c7328c0661fbee34c0423
Filip Berglund <filbe882@student.liu.se>
2020-02-10 17:07:30 +0100

    Improved the readability of the highscores

--- src/ScoreHandler.py
+++ src/ScoreHandler.py
@@ -22,29 +22,32 @@ class ScoreHandler:
     def view_highscores(self, number_of_scores = 10):
         scores_file = open(self.scores_file_name, "r")
         scores = scores_file.readlines()
+        latest = scores[-1]
         scores.sort(key = first_column, reverse = True)
+        latest_pos = scores.index(latest) + 1
         if scores == []:
             return ""
-        output_string = \
-                "*"*40+"\n" \
-                "    SCORE        DATE\n"
+        output_string = "    SCORE        DATE\n"
         for i in range(min(len(scores),number_of_scores)):
             output_string += \
                     " %d:" % (i + 1) + " "*(7-len(str(first_column(scores[i]))+str(i+1))) + \
-                    scores[i].replace("\n","  *").replace(",","       ") + "\n"
-        output_string  += "\n" + "*"*40+"\n"
-        return output_string
+                    scores[i].replace("\n","").replace(",","       ") + "\n"
+        latest = \
+                " %d:" % latest_pos + " "*(7-len(str(first_column(latest))+str(latest_pos))) + \
+                latest.replace("\n","").replace(",","       ")
+        return output_string, latest, latest_pos
 
     def blit_highscores(self, surface,x,y,a_font):
         scores_file = open(self.scores_file_name, "r")
         scores = scores_file.readlines()
         latest = scores[-1].replace("\n","  *").replace(",","       ")
 
-        text = self.view_highscores()
+        text, latest, latest_pos = self.view_highscores()
         lines = re.findall(".*",text)
         for i in range(0,len(lines)):
             y += 20
-            if lines[i].find(latest) == -1:
-                surface.blit(a_font.render(lines[i], False, Constants.WHITE), (x,y))
+            if i == (2*latest_pos):
+                surface.blit(a_font.render(lines[i], False, [240,0,160]), (x,y))
             else:
-                surface.blit(a_font.render("NEW RECORD!%s" % lines[i][1:], False, [240,0,160]), (x-180,y))
+                surface.blit(a_font.render(lines[i], False, Constants.WHITE), (x,y))
+        surface.blit(a_font.render(latest, False, [240,0,160]), (x,y))
--- src/ScoreState.py
+++ src/ScoreState.py
@@ -11,6 +11,11 @@ class ScoreState:
         self.curser_pos = 0
         pygame.font.init()
         self.flappy_font = pygame.font.Font('../res/04B_19__.TTF', 20)
+        self.mono_font = pygame.font.SysFont('Courier', 30)
+        self.background_surface = pygame.transform.scale((pygame.image.load('../res/background.png')), (Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT))
+        self.scoreboard_surface = pygame.Surface((715,500))
+        self.scoreboard_surface.set_alpha(225)
+        self.scoreboard_surface.fill(0)
 
     def input_handler(self):
         for event in pygame.event.get():
@@ -28,9 +33,10 @@ class ScoreState:
                         pygame.quit()
 
     def draw(self):
-        background_surface = pygame.transform.scale((pygame.image.load('../res/background.png')), (Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT))
-        self.window.blit(background_surface, (0, 0))
-        self.score_handler.blit_highscores(self.window,Constants.WINDOW_WIDTH/2-300,20,self.flappy_font)
+        self.window.blit(self.background_surface, (0, 0))
+        self.window.blit(self.scoreboard_surface, (Constants.WINDOW_WIDTH/2-320,20))
+        #pygame.draw.rect(self.window,(0,0,0),(Constants.WINDOW_WIDTH/2-320,20,715,500))
+        self.score_handler.blit_highscores(self.window,Constants.WINDOW_WIDTH/2-300,20,self.mono_font)
         x_pos = 50
         y_pos = Constants.WINDOW_HEIGHT - 120
         textsurface = self.flappy_font.render('MENU', False, Constants.WHITE)
